1077f19f08c7afede054a6944adfb1fa16f73905
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-07-31 20:38:24 -0300

    Displays a flashed message when the user changes their name

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect, abort, render_template, session, url_for
+from flask import Flask, request, make_response, redirect, abort, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime, timezone
@@ -19,6 +19,9 @@ class NameForm(FlaskForm):
 def index():
   form = NameForm()
   if form.validate_on_submit():
+    old_name = session.get('name')
+    if old_name is not None and old_name != form.name.data:
+      flash("Looks like you have changed your name!")
     session['name'] = form.name.data
     return redirect(url_for('index'))
   return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.now(timezone.utc))
