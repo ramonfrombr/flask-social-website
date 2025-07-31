cc0f2d613e49458470de8322910c9981c93227ad
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-07-31 19:21:36 -0300

    Installed flask-moment. Displays current time in homepage using flask-moment

--- hello.py
+++ hello.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, make_response, redirect, abort, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', current_time=datetime.now(timezone.utc))
 
 @app.route('/user/<name>')
 def user(name):
