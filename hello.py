a7a4eecd7f35480f33828bfdb7a9160ee6573e60
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-08-01 12:27:19 -0300

    Creates User rows in the database when the client submits a new username. Displays personalized greeting depending on whether the user is new or already exists

--- hello.py
+++ hello.py
@@ -47,12 +47,24 @@ class NameForm(FlaskForm):
 def index():
   form = NameForm()
   if form.validate_on_submit():
-    old_name = session.get('name')
-    if old_name is not None and old_name != form.name.data:
-      flash("Looks like you have changed your name!")
+    user = User.query.filter_by(username=form.name.data).first()
+    if user is None:
+      user = User(username=form.name.data)
+      db.session.add(user)
+      db.session.commit()
+      session['known'] = False
+    else:
+      session['known'] = True
     session['name'] = form.name.data
+    form.name.data = ''
     return redirect(url_for('index'))
-  return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.now(timezone.utc))
+  return render_template(
+    'index.html',
+    form=form,
+    name=session.get('name'),
+    known=session.get('known', False),
+    current_time=datetime.now(timezone.utc)
+  )
 
 @app.route('/user/<name>')
 def user(name):
