eb4b61b8c7081b78cb3f3a22da4968cc758da6ba
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-08-02 11:18:12 -0300

    Created auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 
+
 def create_app(config_name):
   app = Flask(__name__)
   app.config.from_object(config[config_name])
@@ -22,5 +23,8 @@ def create_app(config_name):
 
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
+
+  from .auth import auth as auth_blueprint
+  app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
   return app
