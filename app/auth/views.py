be963329a2fd65db77b56d0fc96b8e958260d5d4
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-08-02 19:28:09 -0300

    Implemented a resend confirmation email route for unauthenticated users

--- app/auth/views.py
+++ app/auth/views.py
@@ -43,7 +43,7 @@ def register():
     send_email(user.email, 'Confirm Your Account',
                'auth/email/confirm', user=user, token=token)
     flash('A confirmation email has been sent to you by email.')
-    return redirect(url_for('auth.index'))
+    return redirect(url_for('main.index'))
   return render_template("auth/register.html", form=form)
 
 
@@ -74,3 +74,13 @@ def unconfirmed():
   if current_user.is_anonymous or current_user.confirmed:
     return redirect(url_for('main.index'))
   return render_template('auth/unconfirmed.html')
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+  token = current_user.generate_confirmation_token()
+  send_email(current_user.email, 'Confirm Your Account',
+             'auth/email/confirm', user=current_user, token=token)
+  flash('A new confirmation email has been sent to you by email.')
+  return redirect(url_for('main.index'))
