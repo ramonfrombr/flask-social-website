de6f775d6a1bb92a5b9ea777a86983bdf86657e5
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-08-03 20:40:57 -0300

    Implemented edit profile functionality

--- app/main/views.py
+++ app/main/views.py
@@ -1,40 +1,18 @@
 from datetime import datetime, timezone
-from flask import render_template, session, redirect, url_for, current_app
+from flask import flash, render_template, session, redirect, url_for, current_app
 
 from app.decorators import admin_required, permission_required
 from . import main
-from .forms import NameForm
+from .forms import EditProfileForm
 from .. import db
 from ..models import Permission, User
 from ..email import send_email
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-  form = NameForm()
-  if form.validate_on_submit():
-    user = User.query.filter_by(username=form.name.data).first()
-    if user is None:
-      user = User(username=form.name.data)
-      db.session.add(user)
-      db.session.commit()
-      session['known'] = False
-      if current_app.config['APP_ADMIN']:
-        send_email(current_app.config['APP_ADMIN'],
-                   'New User', 'mail/new_user', user=user)
-    else:
-      session['known'] = True
-    session['name'] = form.name.data
-    form.name.data = ''
-    return redirect(url_for('.index'))
-  return render_template(
-      'index.html',
-      form=form,
-      name=session.get('name'),
-      known=session.get('known', False),
-      current_time=datetime.now(timezone.utc)
-  )
+  return render_template('index.html')
 
 
 @main.route('/secret')
@@ -61,3 +39,21 @@ def for_moderators_only():
 def user(username):
   user = User.query.filter_by(username=username).first_or_404()
   return render_template('user.html', user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+  form = EditProfileForm()
+  if form.validate_on_submit():
+    current_user.name = form.name.data
+    current_user.location = form.location.data
+    current_user.about_me = form.about_me.data
+    db.session.add(current_user._get_current_object())
+    db.session.commit()
+    flash('Your profile has been updated.')
+    return redirect(url_for('.user', username=current_user.username))
+  form.name.data = current_user.name
+  form.location.data = current_user.location
+  form.about_me.data = current_user.about_me
+  return render_template('edit_profile.html', form=form)
