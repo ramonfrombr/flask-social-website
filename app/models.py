8e24c1f5b128275f4b67dbc55630c0701dcfa3ef
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-08-05 18:54:05 -0300

    Created Comment model, created and applied migrations

--- app/models.py
+++ app/models.py
@@ -95,6 +95,7 @@ class User(UserMixin, db.Model):
                               backref=db.backref('followed', lazy='joined'),
                               lazy='dynamic',
                               cascade='all, delete-orphan')
+  comments = db.relationship('Comment', backref='author', lazy='dynamic')
 
   @property
   def followed_posts(self):
@@ -244,6 +245,8 @@ class Post(db.Model):
                         default=lambda: datetime.now(timezone.utc))
   author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
+  comments = db.relationship('Comment', backref='post', lazy='dynamic')
+
   @staticmethod
   def on_changed_body(target, value, oldvalue, initiator):
     allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
@@ -265,7 +268,27 @@ class Follow(db.Model):
       db.DateTime, default=lambda: datetime.now(timezone.utc))
 
 
+class Comment(db.Model):
+  __tablename__ = 'comments'
+  id = db.Column(db.Integer, primary_key=True)
+  body = db.Column(db.Text)
+  body_html = db.Column(db.Text)
+  timestamp = db.Column(db.DateTime, index=True,
+                        default=lambda: datetime.now(timezone.utc))
+  disabled = db.Column(db.Boolean)
+  author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+  @staticmethod
+  def on_changed_body(target, value, oldvalue, initiator):
+    allowed_tags = ['a', 'abbr', 'acronym', 'b', 'code', 'em', 'i', 'strong']
+    target.body_html = bleach.linkify(bleach.clean(
+        markdown(value, output_format='html'),
+        tags=allowed_tags, strip=True))
+
+
 db.event.listen(Post.body, 'set', Post.on_changed_body)
+db.event.listen(Comment.body, 'set', Comment.on_changed_body)
 
 
 class AnonymousUser(AnonymousUserMixin):
