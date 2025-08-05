8d1e3125793179ee451cbe3383f69b72159146d0
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-08-05 11:59:11 -0300

    Implemented tabs for all posts and followed posts on index route

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-from flask import abort, flash, render_template, session, redirect, url_for, current_app, request
+from flask import abort, flash, make_response, render_template, session, redirect, url_for, current_app, request
 
 from app.decorators import admin_required, permission_required
 from . import main
@@ -19,11 +19,40 @@ def index():
     db.session.commit()
     return redirect(url_for('.index'))
   page = request.args.get('page', 1, type=int)
-  pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+  show_followed = False
+  if current_user.is_authenticated:
+    show_followed = bool(request.cookies.get('show_followed', ''))
+  if show_followed:
+    query = current_user.followed_posts
+  else:
+    query = Post.query
+  pagination = query.order_by(Post.timestamp.desc()).paginate(
       page=page, per_page=current_app.config['APP_POSTS_PER_PAGE'],
       error_out=False)
   posts = pagination.items
-  return render_template('index.html', form=form, posts=posts, pagination=pagination)
+  return render_template(
+      'index.html',
+      form=form,
+      posts=posts,
+      show_followed=show_followed,
+      pagination=pagination
+  )
+
+
+@main.route('/all')
+@login_required
+def show_all():
+  response = make_response(redirect(url_for('.index')))
+  response.set_cookie('show_followed', '', max_age=30 * 24 * 60 * 60)
+  return response
+
+
+@main.route('/followed')
+@login_required
+def show_followed():
+  response = make_response(redirect(url_for('.index')))
+  response.set_cookie('show_followed', '1', max_age=30 * 24 * 60 * 60)
+  return response
 
 
 @main.route('/secret')
