fe1a25208f5645f914b3ca128cdddfc8d2063796
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-08-08 21:14:38 -0300

    Implemented a register and login test

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,5 @@
 import unittest
+import re
 from app import create_app, db
 from app.models import User, Role
 
@@ -21,3 +22,40 @@ class FlaskClientTestCase(unittest.TestCase):
     response = self.client.get('/')
     self.assertEqual(response.status_code, 200)
     self.assertTrue('Stranger' in response.get_data(as_text=True))
+
+  def test_register_and_login(self):
+    # register a new account
+    response = self.client.post('/auth/register', data={
+        'email': 'john@example.com',
+        'username': 'john',
+        'password': 'cat',
+        'password2': 'cat'
+    })
+    self.assertEqual(response.status_code, 302)
+
+    # log in with the new account
+    response = self.client.post('/auth/login', data={
+        'email': 'john@example.com',
+        'password': 'cat'
+    }, follow_redirects=True)
+    self.assertEqual(response.status_code, 200)
+    self.assertTrue(re.search('Hello,\s+john!',
+                    response.get_data(as_text=True)))
+    self.assertTrue(
+        'You have not confirmed your account yet' in response.get_data(as_text=True))
+
+    # send a confirmation token
+    user = User.query.filter_by(email='john@example.com').first()
+    token = user.generate_confirmation_token()
+    response = self.client.get(
+        '/auth/confirm/{}'.format(token), follow_redirects=True)
+    user.confirm(token)
+    self.assertEqual(response.status_code, 200)
+    self.assertTrue(
+        'You have confirmed your account' in response.get_data(as_text=True))
+
+    # log out
+    response = self.client.get('/auth/logout', follow_redirects=True)
+    self.assertEqual(response.status_code, 200)
+    self.assertTrue(
+        'You have been logged out' in response.get_data(as_text=True))
