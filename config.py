6b034aed8d4ace82b0174cab9ff62050335c3b12
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-08-10 16:02:43 -0300

    Updated User model. Implemented logging errors during production

--- config.py
+++ config.py
@@ -39,6 +39,29 @@ class ProductionConfig(Config):
   SQLALCHEMY_DATABASE_URI = os.environ.get(
       'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
+  @classmethod
+  def init_app(cls, app):
+    Config.init_app(app)
+    # email errors to administrators`
+    import logging
+    from logging.handlers import SMTPHandler
+    credentials = None
+    secure = None
+    if getattr(cls, 'MAIL_USERNAME', None) is not None:
+      credentials = (cls.MAIL_USERNAME, cls. MAIL_PASSWORD)
+      if getattr(cls, 'MAIL_USE_TLS', None):
+        secure = ()
+    mail_handler = SMTPHandler(
+        mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+        fromaddr=cls.MAIL_SENDER,
+        toaddrs=[cls.APP_ADMIN],
+        subject='[Social Website] Application Error',
+        credentials=credentials,
+        secure=secure
+    )
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+
 
 config = {
     'development': DevelopmentConfig,
